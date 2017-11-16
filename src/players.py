b7d9079a0e9a769870464a0eede1e7385f7cb077
Asim Unmesh <a.unmesh@gmail.com>
2017-11-17 01:11:27 +0530

    Constructor Created for Agents

--- src/players.py
+++ src/players.py
@@ -1,1 +1,22 @@
 import simpy
+import numpy as np
+
+
+class Agent(object):
+
+	'''Constructor for the super class Agent
+		Args:
+			id: Name of Agent
+			Occupation: Farmers or Consumers
+			True_Type: True Type of Agent
+			Reported_Type: Reported type of Agent
+			Local_Distance: Local Distance of Agent from their market(to be used in the mechanism)
+	'''			
+
+	def __init__(self, id, Occupation, True_Type, Reported_Type,local_distance, env):
+		self.id = id
+		self.Occupation = Occupation
+		self.true_type = True_Type  
+		self.rep_type = Reported_Type
+		self.loc_dist = local_distance
+		self.env = env
