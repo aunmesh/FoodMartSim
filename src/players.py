c79a4311ef53c612084e64ece73997f045f022dc
kunalthegreat <kunalch@iitk.ac.in>
2017-11-17 02:27:24 +0530

    player constructers done

--- src/players.py
+++ src/players.py
@@ -1,5 +1,6 @@
 import simpy
 import numpy as np
+import random
 
 
 class Agent(object):
@@ -7,16 +8,29 @@ class Agent(object):
 	'''Constructor for the super class Agent
 		Args:
 			id: Name of Agent
-			Occupation: Farmers or Consumers
 			True_Type: True Type of Agent
-			Reported_Type: Reported type of Agent
+			bid: Bid of Agent
 			Local_Distance: Local Distance of Agent from their market(to be used in the mechanism)
-	'''			
+	'''
 
-	def __init__(self, id, Occupation, True_Type, Reported_Type,local_distance, env):
-		self.id = id
-		self.Occupation = Occupation
-		self.true_type = True_Type  
-		self.rep_type = Reported_Type
+	def __init__(self, id, bid,local_distance, env):
+		self.id = id		
+		self.bid = bid
 		self.loc_dist = local_distance
 		self.env = env
+
+class Farmer(Agent):
+    """docstring for Farmer."""
+
+    def __init__(self, id, bid, local_distance, env):
+        super(Farmer, self).__init__(self, id, bid, local_distance, env)
+        self.true_type=max(0,random.gauss(60,40))
+        self.qty=random.uniform(0,20)
+
+class Buyer(Agent):
+    """docstring for Buyer."""
+
+    def __init__(self, id, bid, local_distance, env):
+        super(Buyer, self).__init__(self, id, True_Type, bid, local_distance, env)
+        self.true_type=max(0,random.gauss(100,40))
+        self.qty=random.uniform(0,20)
---
+++ src/sim.py
@@ -0,0 +1,20 @@
+import simpy
+import numpy as np
+import Market
+import player
+
+def allocate():
+
+def checkDSIC():
+
+def calculatepayments():
+
+def
+
+
+def main():
+    
+
+
+if __name__ == '__main__':
+    main()
