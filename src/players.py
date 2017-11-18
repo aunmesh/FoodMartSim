a266bf87a807149aa2225d74992a733134ca22fe
Asim Unmesh <a.unmesh@gmail.com>
2017-11-18 18:52:19 +0530

    Artifacts removed

--- src/players.py
+++ src/players.py
@@ -17,17 +17,17 @@ class Agent(object):
 		self.env = env
 		self.dead = False
 
-<<<<<<< Updated upstream
+
 	def __init__(self, Id, env):
 		self.id = Id
 		self.loc_dist = math.floor(random.uniform(1,50))
-=======
+
 '''
 	def __init__(self, Id, local_distance, env):
 		self.id = Id
 		self.env = env
 		self.loc_dist = random.uniform(1,50)
->>>>>>> Stashed changes
+
 		self.qty_traded = 0
 		self.payment = 0
 
@@ -40,13 +40,13 @@ class Farmer(Agent):
 	brk_index = 0 #static variable to store the break even index
 
 	def __init__(self, Id, env):
-<<<<<<< Updated upstream
+
 		super(Farmer, self).__init__(self, Id, env)
 
 		self.true_type = math.floor(max(1,random.gauss(60,40)))
 		self.bid = math.floor(max(true_type,random.gauss(true_type+10,40)))
 		self.qty = math.floor(random.uniform(1,20))
-=======
+
 
 		super(Farmer, self).__init__(self, Id, env)
 		#super(Farmer, self).__init__(self, Id, bid, local_distance, env)
@@ -55,7 +55,7 @@ class Farmer(Agent):
 		self.bid = max(true_type,random.gauss(true_type+10,40))
 		self.qty = random.uniform(0,20)
 
->>>>>>> Stashed changes
+
 		self.action = self.env.process(self.run())
 
 		self.loc_dist = random.uniform(1,50)
