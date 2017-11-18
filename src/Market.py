33e5f0c2b031b69cf248af82ce1e57f3e22c7e8e
Asim Unmesh <a.unmesh@gmail.com>
2017-11-18 19:11:52 +0530

    Modified add_agent method

--- src/Market.py
+++ src/Market.py
@@ -3,7 +3,6 @@ import numpy as np
 
 #Allocate function is assumed to contain the Allocate function
 from Allocate import *
-
 from players import *
 
 class Market(object):
@@ -15,12 +14,17 @@ class Market(object):
 		farmer_pop - The farmer population
 		buyer_pop - The buyer population
 	'''
-
 	def __init__(self,env,name, farmer_pop, buyer_pop):
+
 		self.env = env
 		self.name = name
 		self.farmer_pop=farmer_pop
 		self.buyer_pop=buyer_pop
+
+		#Keeps the position where new ids are to be assigned from
+
+		self.FARMER_IDX = 0
+		self.BUYER_IDX = 0
 
 		#List of all farmer and buyer objects.
 		self.farmers = []
@@ -31,20 +35,34 @@ class Market(object):
 			#temp_bid = #Sample from some distribution
 			self.farmers.append(Farmer(temp_id, self.env))
 
-        for temp in range(buyer_pop):
+		#Updating Farmer ID which is available
+		self.FARMER_IDX+=len(farmer_pop)
+
+		for temp in range(buyer_pop):
             #temp_bid = #Sample from some distribution
             self.buyers.append(Buyer(temp_id, self.env))
 
+		#Updating Buyer ID which is available
+		self.BUYER_IDX+=len(buyer_pop)
 
-	#method to add agent before trading starts for the day
-	# used when new farmers or buyers are added into the sim
-	def Add_agent(Occupation):
-		if Occupation==0: #farmer
-			self.farmers.append(Farmer(farmer_pop, self.env))
-			seller_pop+=1
-		elif Occupation==1:
-			self.buyers.append(Buyer(buyer_pop,self.env))
-			buyer_pop+=1
+
+	#Method to add agent before trading starts for the day
+	#used when new farmers or buyers are added into the sim
+	def Add_agent(Nums):
+		temp_farmer = Nums[0]
+		temp_buyer = Nums[0]
+
+		for temp in range(temp_farmer):
+			self.farmers.append(Farmer(FARMER_IDX + temp , self.env))
+
+		#Updating available ID
+		self.FARMER_IDX+=temp_farmer
+
+		for temp in range(temp_buyer):
+			self.buyers.append(Buyer(BUYER_IDX + temp , self.env))
+
+		#Updating available ID
+		self.BUYER_IDX+=temp_buyer
 
 
 	# TradingDay is the function which will be called by the simpy simulator. It should carry out all the activities the market does in a trading day.
