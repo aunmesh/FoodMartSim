1d3f33461e7918029d15456c2b9e1c850c6d7bdd
kunalthegreat <kunalch@iitk.ac.in>
2017-11-18 19:59:51 +0530

    other updates : perform allocation

--- src/util.py
+++ src/util.py
@@ -22,28 +22,28 @@ def UpdateAgents(farmers,buyers,market):
 			Remove_agent(temp)
 			buyers.remove(temp)
 
-	Add_agent(flen1-flen2,blen1-blen2)
+	Add_agent(market, [ flen1-flen2 , blen1-blen2 ])
 
 
 
 #Method to add agent before trading starts for the day
 #used when new farmers or buyers are added into the sim
 # Nums is list of (no of farmers to be added, no of buyers to be added)
-def Add_agent(Nums):
+def Add_agent(market,Nums):
 	temp_farmer = Nums[0]
 	temp_buyer = Nums[0]
 
 	for temp in range(temp_farmer):
-		self.farmers.append(Farmer(FARMER_IDX + temp , self.env))
+		market.farmers.append(Farmer(FARMER_IDX + temp , market.env))
 
 	#Updating available ID
-	self.FARMER_IDX+=temp_farmer
+	market.FARMER_IDX+=temp_farmer
 
 	for temp in range(temp_buyer):
-		self.buyers.append(Buyer(BUYER_IDX + temp , self.env))
+		market.buyers.append(Buyer(BUYER_IDX + temp , market.env))
 
 	#Updating available ID
-	self.BUYER_IDX+=temp_buyer
+	market.BUYER_IDX+=temp_buyer
 
 
 
@@ -55,42 +55,38 @@ def Remove_agent(agent):
 
 # Method to perform Allocations in a single pass over the farmers and buyers list
 def PerformAllocations(market,Allocations):
-	for i in range(seller_pop):
-		for j in range(buyer_pop):
+	for i in range(market.farmer_pop):
+		for j in range(market.buyer_pop):
 			#decrease farmer quantity by the allocated value
 			# decrease buyer quantity by the allocated value
-			farmers[i].qty = farmers[i].qty - allocation[i][j]
-			buyers[j].qty = buyers[j].qty - allocation[i][j]
+			market.farmers[i].qty = market.farmers[i].qty - allocation[i][j]
+			market.buyers[j].qty = marekt.buyers[j].qty - allocation[i][j]
 
 
 
 
 #return a matrix of what is allocated to whom
-<<<<<<< HEAD
 # ouput is (farmer_pop x buyer_pop) matrix where each element is quantity traded between them
-def RunMechanism():
-=======
 def RunMechanism(farmers, buyers):
->>>>>>> d2e196dbc1c6dff20263dd17f81571f79da2141d
   # computes allocation and payment
-  seller_pop = len(farmers)
+  farmer_pop = len(farmers)
   buyer_pop = len(buyers)
   farmers.sort(key=lambda x: x.bid, reverse=False)
   buyers.sort(key=lambda x: x.bid, reverse=True)
-<<<<<<< HEAD
 
-  allocation = [[0 for x in range(seller_pop)] for y in range(buyer_pop)] # 2D matrix to store quantity traded between each farmer and buyer
-  for i in range(seller_pop): # duplicating the supply and demand
+  allocation = [[0 for x in range(farmer_pop)] for y in range(buyer_pop)]
+  # 2D matrix to store quantity traded between each farmer and buyer
+  sell = [0 for x in range(farmer_pop)]
+  buy = [0 for x in range(buyer_pop)]
+  for i in range(farmer_pop): # duplicating the supply and demand
       farmers[i].qty_traded=0
-=======
->>>>>>> d2e196dbc1c6dff20263dd17f81571f79da2141d
       sell[i] = farmers[i].qty
   for i in range(buyer_pop):
 	  buyers[i].qty_traded=0
       buy[i] = buyers[i].qty
   s = 0
   b = 0
-  while(s < seller_pop - 1 and b < buyer_pop - 1):
+  while(s < farmer_pop - 1 and b < buyer_pop - 1):
       q = min(sell[s], buy[b])
       sell[s] -= q
       buy[b] -= q
@@ -109,12 +105,12 @@ def RunMechanism(farmers, buyers):
           allocation[s][b] = q
           s += ds
           b += db
-  for i in range(seller_pop):
+  for i in range(farmer_pop):
       for j in range(buyer_pop):
           farmers[i].qty_traded += allocation[i][j]
           buyers[j].qty_traded += allocation[i][j]
 
-  for i in range(seller_pop):
+  for i in range(farmer_pop):
       if(not farmers[i].qty_traded):
           Farmer.brk_index = i
           break
